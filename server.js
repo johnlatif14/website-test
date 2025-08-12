ccde01308b8c5832bd59635cb17952ed52dab2b6
johnlatif2026 <youremail@example.com>
2025-08-12 03:52:47 +0300

    أول رفع

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const requireAuth = (req, res, next) => {
 };
 
 // الصفحات الرئيسية
-app.get('/contact.html', (req, res) => {
+app.get('/public/contact.html', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'contact.html'));
 });
 
@@ -60,7 +60,7 @@ app.post('/login', (req, res) => {
   
   if (username === process.env.ADMIN_USERNAME && password === process.env.ADMIN_PASSWORD) {
     req.session.isAuthenticated = true;
-    res.redirect('/dashboard.html');
+    res.redirect('/dashboard');
   } else {
     res.redirect('/login?error=invalid_credentials');
   }
